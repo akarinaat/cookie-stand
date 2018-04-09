8269aad06d2db2325c76c35a1cdb8525d7c5f1e6
Karina Amador <akarina.at@gmail.com>
2018-04-09 16:19:50 -0700

    I added another css file for the html file

--- js/app.js
+++ js/app.js
@@ -32,25 +32,4 @@ var firstandPike = {
 firstandPike.cookieSalesPerHour();
 console.log(firstandPike.dailySales);
 
-var months = ['May', 'June', 'July', 'August', 'September'];
-
-var twoThousandSeventeen = {
-  miles: [16, 43, 653, 19, 2],
-  renderMiles: function() {
-    // access the parent element from the DOM
-    var ulElement = document.getElementById('seventeen');
-  
-    for(var i = 0; i < months.length; i++) {
-      // 1. create element
-      var listItemElement = document.createElement('li');
-  
-      // 2. give it content
-      listItemElement.textContent = months[i] + ': ' + this.miles[i];
-  
-      // 3. append it to the DOM
-      // parentElement.appendChild(childElement);
-      ulElement.appendChild(listItemElement);
-    }
-  }
-};
 
