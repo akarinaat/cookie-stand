fa6e0b695516ff46cbca4a440e77f13cc0ad9f97
Karina Amador <akarina.at@gmail.com>
2018-04-09 20:10:51 -0700

    Second store plus the total of cookies. I also added some css to the index

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var storeHours = [6 + 'am', 7 + 'am', 8 + 'am', 9 + 'am', 10 + 'am', 11 + 'am', 12 + 'pm', 1 + 'pm', 2 + 'pm', 3 + 'pm', 4 + 'pm', 5 + 'pm', 6 + 'pm', 7 + 'pm', 8 + 'pm'];
 
-
+//PIKE LOCATION
 var firstandPike = {
   minClientPerHour: 23,
   maxClientPerHour: 65,
@@ -26,8 +26,55 @@ var firstandPike = {
       this.dailySales += numberSold;
     }
   },
+
   renderSales: function (){
-    var ulElem = document.getElementById('sales');
+    var ulElem = document.getElementById('salesinpike');
+    for (var i = 0; i < storeHours.length; i++){
+      //create an element
+      var listItemElem = document.createElement( 'li' );
+      //Give it a content
+      listItemElem.textContent = storeHours[i] + ': ' + Math.round(this.cookiesPerHour[i]) + ' cookies.';
+      //Append to the DOM
+      ulElem.appendChild(listItemElem);
+
+    }
+    listItemElem.textContent = 'Total of cookies: ' + Math.round(this.dailySales);
+  }
+
+};
+
+firstandPike.randomNumberOfClient();
+firstandPike.cookieSalesPerHour();
+firstandPike.renderSales();
+console.log(firstandPike.dailySales);
+
+
+//SeaTac Airport LOCATION
+var seaTac = {
+  minClientPerHour: 3,
+  maxClientPerHour: 24,
+  averageCookiesPerCustomer: 1.2,
+  dailySales:0,
+  cookiesPerHour:[],
+  customerPerHour: [],
+
+  randomNumberOfClient: function ()//calculates the random clients
+  {
+    for (var i = 0; i < storeHours.length; i++){
+
+      this.customerPerHour[i] = Math.floor(Math.random() * (this.maxClientPerHour - this.minClientPerHour)) + this.minClientPerHour;
+    }
+  },
+  cookieSalesPerHour: function () //calculates sales per hour and total sales
+  {
+    for(var i = 0; i <storeHours.length; i++ ){
+      var numberSold = this.customerPerHour[i] * this.averageCookiesPerCustomer;
+      this.cookiesPerHour[i] = numberSold;
+      this.dailySales += numberSold;
+    }
+  },
+  renderSales: function (){
+    var ulElem = document.getElementById('seaTacAir');
     for (var i = 0; i < storeHours.length; i++){
       //create an element
       var listItemElem = document.createElement( 'li' );
@@ -37,13 +84,20 @@ var firstandPike = {
       ulElem.appendChild(listItemElem);
 
     }
+    listItemElem.textContent = 'Total of cookies: ' + Math.round(this.dailySales) + ' cookies.';
   }
 
 };
 
-firstandPike.randomNumberOfClient();
-firstandPike.cookieSalesPerHour();
-firstandPike.renderSales();
-console.log(firstandPike.dailySales);
+seaTac.randomNumberOfClient();
+seaTac.cookieSalesPerHour();
+seaTac.renderSales();
+console.log(seaTac.dailySales);
+
+//Seattle Center LOCATION
+
+var seattleCenter
+
+
 
 
