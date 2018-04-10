411f23465d47d7b84d3266d230c6e7b8c3b22608
Karina Amador <akarina.at@gmail.com>
2018-04-09 17:01:11 -0700

    This is the example of the first store, I still need to do the DOM part

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 var storeHours = [6 + 'am', 7 + 'am', 8 + 'am', 9 + 'am', 10 + 'am', 11 + 'am', 12 + 'pm', 1 + 'pm', 2 + 'pm', 3 + 'pm', 4 + 'pm', 5 + 'pm', 6 + 'pm', 7 + 'pm', 8 + 'pm'];
 
 
-
 var firstandPike = {
   minClientPerHour: 23,
   maxClientPerHour: 65,
@@ -12,7 +11,8 @@ var firstandPike = {
   cookiesPerHour:[],
   customerPerHour: [],
 
-  randomNumberOfClient: function (){
+  randomNumberOfClient: function ()//calculates
+  {
     for (var i = 0; i < storeHours.length; i++){
 
       this.customerPerHour[i] = Math.floor(Math.random() * (this.maxClientPerHour - this.minClientPerHour)) + this.minClientPerHour;
@@ -29,7 +29,8 @@ var firstandPike = {
 
 };
 
-firstandPike.cookieSalesPerHour();
+
+firstandPike();
 console.log(firstandPike.dailySales);
 
 
