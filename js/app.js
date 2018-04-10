4d1446eebf63e448105faf80e36b74b93ba753f2
Karina Amador <akarina.at@gmail.com>
2018-04-10 14:37:11 -0700

    Almost donde with the index.html style

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 var storeHours = [6 + 'am', 7 + 'am', 8 + 'am', 9 + 'am', 10 + 'am', 11 + 'am', 12 + 'pm', 1 + 'pm', 2 + 'pm', 3 + 'pm', 4 + 'pm', 5 + 'pm', 6 + 'pm', 7 + 'pm', 8 + 'pm'];
 
+function LocationSales (){
+   
+}
+
 //PIKE LOCATION
 var firstandPike = {
   minClientPerHour: 23,
